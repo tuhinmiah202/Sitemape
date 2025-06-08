7c461f1bb8ab775ae330b7485b5616a26bbc77b7
tuhinmiah202 <tuhinmiah2004@gmail.com>
2025-06-09 00:17:42 +0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const sitemapRoute = require('./routes/sitemap');
+app.use('/', sitemapRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
